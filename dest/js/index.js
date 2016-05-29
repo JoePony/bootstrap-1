ddb2e02c553e4bb4ff7a2e4fefc1204c5f17e982
joepony <5157222212@qq.com>
2016-05-29 07:59:40 -0700

    second commit

--- dest/js/index.js
+++
@@ -1,1 +0,0 @@
-define(["jquery"],function(){return{execute:function(){$(".alertModal").on("click",function(){$("#modalAbout").modal("toggle"),$("#modalAbout h4").html("'"+$(this).text()+"'"+" 即将上线，敬请期待...")}),$(".foldForm").on("click",function(a){$("form").slideToggle(100,function(){$(".foldForm").toggle()}),a.preventDefault()})}}});
